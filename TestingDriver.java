a5f803cc74246c5e89a0dedffc9535b81696e381
ATyagi-LU <a.tyagi2@lancaster.ac.uk>
2023-05-13 13:40:11 +0100

    The test for making sure that the driver class for testing works

--- TestingDriver.java
+++ TestingDriver.java
@@ -1,7 +1,8 @@
 public class TestingDriver
 {
     public static void main(String[] args)
-    {
-
+    {   
+        FileReader fileReader = new FileReader();
+        System.out.println(fileReader.getPath());
     }
 }
