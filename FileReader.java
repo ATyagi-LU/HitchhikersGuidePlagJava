b0c3822e6a8ae51490c79c069299b68251ac34b4
ATyagi-LU <a.tyagi2@lancaster.ac.uk>
2023-05-15 13:09:03 +0100

    Updated functions in FileReader Now an arraylist of hashmaps exists for the text files getter created testing in TestingDriver updated

--- FileReader.java
+++ FileReader.java
@@ -1,17 +1,58 @@
-import java.util.ArrayList;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.HashMap;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
 
 public class FileReader
 {
     private int fileCount;
-    private ArrayList<String> files = new ArrayList<String>();
+    private String[] files;
+    private ArrayList<Map<String, Integer>> arr = new ArrayList<Map<String, Integer>>();
 
-    String getPath(){
+    String getPath()
+    {
 
         Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString();
         return s;
+    }
+
+    void getTxtFiles(String path)
+    {
+        File f = new File(path);
+        
+        FilenameFilter filter = new FilenameFilter() {
+            @Override
+            public boolean accept(File f, String name) 
+            {
+                return name.endsWith(".txt");
+            }
+        };
+        files = f.list(filter);
+
+        for (String file : files) {
+            fileCount++;
+            System.out.println(file);
+        }
+    }
+
+    void wordCountingMaps(){
+        
+        for (int i = 0; i < fileCount; i++)
+        {
+            HashMap<String,Integer> file = new HashMap<String, Integer>();
+            arr.add(file);
+        }
+    }
+
+    public ArrayList<Map<String, Integer>> getArr() {
+        return arr;
     }
 
 }
--- TestingDriver.java
+++ TestingDriver.java
@@ -1,8 +1,21 @@
+import java.util.Map;
+import java.util.ArrayList;
+
+
 public class TestingDriver
 {
     public static void main(String[] args)
     {   
         FileReader fileReader = new FileReader();
         System.out.println(fileReader.getPath());
+        fileReader.getTxtFiles(fileReader.getPath());
+        fileReader.wordCountingMaps();
+        ArrayList<Map<String, Integer>> jeff = fileReader.getArr();
+        int length = jeff.size();
+        for(int i = 0; i<length; i++)
+        {
+            System.out.println(jeff.get(i).isEmpty());
+        }
+
     }
 }
