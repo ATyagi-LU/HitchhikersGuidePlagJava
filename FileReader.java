bc36f0d900f3aaa7b90f6723e5c7978971dca397
ATyagi-LU <a.tyagi2@lancaster.ac.uk>
2023-05-17 15:24:45 +0100

    Removal of dummy files, File pair function working TODO: check pairs are correct

--- FileReader.java
+++ FileReader.java
@@ -10,11 +10,68 @@ import java.util.ArrayList;
 public class FileReader {
     private int fileCount;
     private String[] files;
-    private ArrayList<HashMap<String, Integer>> arr = new ArrayList<HashMap<String, Integer>>();
+    private ArrayList<HashMap<String, Integer>> wordCountMaps = new ArrayList<HashMap<String, Integer>>();
     private ArrayList<String[]> splitTexts = new ArrayList<String[]>();
     private ArrayList<String> texts = new ArrayList<String>();
     private int phraseMinWordCount;
-    
+    private ArrayList<String[]> filePairs = new ArrayList<String[]>();
+    private HashMap<String, Double> matchPercents = new HashMap<String, Double>();
+
+    int findIndexInFiles(String keyword){
+        int counter = 0;
+        for (String file : files){
+            if (file == keyword){
+                return counter;
+            }
+            counter++;    
+        }
+        return -1;
+    }
+
+    void filePairMatches(){
+        for (int i = 0; i < filePairs.size();i++)
+        {
+            String[] fileMatch = filePairs.get(i);
+            String filePair = (fileMatch[0] + "," + fileMatch[1]);
+            int textIndexA = findIndexInFiles(fileMatch[0]);
+            int textIndexB = findIndexInFiles(fileMatch[1]);
+            double matchPercent = phraseMatcher(texts.get(textIndexA), texts.get(textIndexB));
+            matchPercents.put(filePair,matchPercent);
+        }
+    }
+
+    void printMatchPercents(){
+        for (String j : matchPercents.keySet()) {
+            System.out.println("Pair: " + j + "; Percent match for File Pair: " + matchPercents.get(j));
+        }
+    }
+
+    void filePair() {
+        int endIndex = files.length;
+        for(int i = 0; i <endIndex; i++){
+            int j=0;
+            while(j<endIndex){
+                if (j<=i){
+                    j++;
+                }
+                else{
+                    String[] catc = {files[i],files[j]};
+                    filePairs.add(catc);
+                    j++;
+                }
+            }
+        }
+    }
+
+    void printFilePair() {
+        for (int i = 0; i < filePairs.size(); i++) {
+            String[] current = filePairs.get(i);
+            for (String word : current) {
+                System.out.print(word + ",");
+            }
+            System.out.println();
+        }
+    }
 
     String getPath() {
 
@@ -52,12 +109,12 @@ public class FileReader {
 
         for (int i = 0; i < fileCount; i++) {
             HashMap<String, Integer> file = new HashMap<String, Integer>();
-            arr.add(file);
+            wordCountMaps.add(file);
         }
     }
 
     public ArrayList<HashMap<String, Integer>> getArr() {
-        return arr;
+        return wordCountMaps;
     }
 
     String[] fileRead(String filename) {
@@ -91,15 +148,14 @@ public class FileReader {
 
     void wordCounterMapper() {
         for (int i = 0; i < fileCount; i++) {
-            HashMap<String, Integer> currentHashMap = arr.get(i);
+            HashMap<String, Integer> currentHashMap = wordCountMaps.get(i);
             String[] currentText = splitTexts.get(i);
-            for(String word : currentText){
+            for (String word : currentText) {
                 boolean exists = currentHashMap.containsKey(word);
-                if (exists){
+                if (exists) {
                     currentHashMap.put(word, currentHashMap.get(word) + 1);
-                }
-                else{
-                    currentHashMap.put(word,1);
+                } else {
+                    currentHashMap.put(word, 1);
                 }
 
             }
@@ -108,39 +164,35 @@ public class FileReader {
 
     public void setPhraseMinWordCount() {
         boolean valid = false;
-        while(!valid){
-            try{
-                Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        while (!valid) {
+            try {
+                Scanner myObj = new Scanner(System.in); // Create a Scanner object
                 System.out.println("Please input min word count:");
-                this.phraseMinWordCount = Integer.parseInt(myObj.nextLine());
-                if(phraseMinWordCount==0)
-                {   
+                phraseMinWordCount = Integer.parseInt(myObj.nextLine());
+                if (phraseMinWordCount == 0) {
                     System.out.println("Not a valid input, please try again.");
                     valid = false;
                     continue;
                 }
                 myObj.close();
                 valid = true;
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 System.out.println("Not a valid input, please try again.");
             }
         }
-        
+
     }
 
-    double phraseMatcher(String textA, String textB){
-        setPhraseMinWordCount();
+    double phraseMatcher(String textA, String textB) {
         int currentIndex = 0;
         int endIndex = 0;
         int phraseWordCount = 0;
         String splitText;
         String smallText;
-        if (textA.length() > textB.length()){
+        if (textA.length() > textB.length()) {
             splitText = textA;
             smallText = textB;
-        }
-        else{
+        } else {
             splitText = textB;
             smallText = textA;
         }
@@ -148,59 +200,58 @@ public class FileReader {
         String phrase = "";
         ArrayList<String> phrases = new ArrayList<String>();
         Boolean match = false;
-        while (currentIndex < words.length)
-        {
+        while (currentIndex < words.length) {
             String currentWord = words[currentIndex];
 
             if (currentWord.startsWith("'") || currentWord.startsWith("\"")) {
-            int startOfQuoteIndex = currentWord.length() + endIndex;
-            int endOfSingleQuoteIndex = splitText.indexOf("'", startOfQuoteIndex);
-            int endOfDoubleQuoteIndex = splitText.indexOf("\"", startOfQuoteIndex);
+                int startOfQuoteIndex = currentWord.length() + endIndex;
+                int endOfSingleQuoteIndex = splitText.indexOf("'", startOfQuoteIndex);
+                int endOfDoubleQuoteIndex = splitText.indexOf("\"", startOfQuoteIndex);
 
-            // check for closing quotes and skip no. of words
-            if (endOfSingleQuoteIndex > -1) {
-                currentIndex += splitText.substring(endIndex, endOfSingleQuoteIndex).split("\\s+").length;
-            }
-            if (endOfDoubleQuoteIndex > -1) {
-                currentIndex += splitText.substring(endIndex, endOfDoubleQuoteIndex).split("\\s+").length;
-            }
-            continue;
+                // check for closing quotes and skip no. of words
+                if (endOfSingleQuoteIndex > -1) {
+                    currentIndex += splitText.substring(endIndex, endOfSingleQuoteIndex).split("\\s+").length;
+                }
+                if (endOfDoubleQuoteIndex > -1) {
+                    currentIndex += splitText.substring(endIndex, endOfDoubleQuoteIndex).split("\\s+").length;
+                }
+                continue;
             }
 
-            // set starting point for phrase matching  
+            // set starting point for phrase matching
             if (phraseWordCount < phraseMinWordCount) {
-            phrase += currentWord + " ";
-            endIndex += currentWord.length() + 1;
-            phraseWordCount++;
-            currentIndex++;
-            continue;
+                phrase += currentWord + " ";
+                endIndex += currentWord.length() + 1;
+                phraseWordCount++;
+                currentIndex++;
+                continue;
             }
-            
+
             // once we have a min length phrase, we can start matching
             // find the current phrase in the following content.
             int foundphraseMatchIndex = smallText.indexOf(phrase);
 
             if (foundphraseMatchIndex > -1) {
-            // if we find a match, we expand the phrase
-            phrase += currentWord + " ";
-            endIndex += currentWord.length() + 1;
-            phraseWordCount++;
-            currentIndex++;
-            match = true;
+                // if we find a match, we expand the phrase
+                phrase += currentWord + " ";
+                endIndex += currentWord.length() + 1;
+                phraseWordCount++;
+                currentIndex++;
+                match = true;
 
-            // If its the end of the sentence then reset the phrase search
-            if (currentWord.endsWith(".")) {
-                phrases.add(phrase.replaceAll("\\.(?=\\s|$)", ""));        
-                match = false;
-                phrase = "";
-                phraseWordCount = 0;
+                // If its the end of the sentence then reset the phrase search
+                if (currentWord.endsWith(".")) {
+                    phrases.add(phrase.replaceAll("\\.(?=\\s|$)", ""));
+                    match = false;
+                    phrase = "";
+                    phraseWordCount = 0;
+                }
+
+                continue;
             }
 
-            continue;
-            }
-            
             if (match) {
-            phrases.add(phrase);
+                phrases.add(phrase);
             }
 
             // move to next phrase
@@ -208,28 +259,23 @@ public class FileReader {
             phraseWordCount--;
             match = false;
         }
-
-        System.out.println("Matches:");
-        for (String p : phrases) {
-            System.out.println(p);
-        }
         String matches = "";
-        for (int i = 0; i<phrases.size();i++){
+        for (int i = 0; i < phrases.size(); i++) {
             matches += phrases.get(i) + " ";
         }
         String[] matchesLength = matches.split("\\s+");
         int matchNum = matchesLength.length;
-        double matchPercent = Math.round((((double)matchNum)/((double)splitText.split("\\s+").length))*100.0);
+        double matchPercent = Math.round((((double) matchNum) / ((double) splitText.split("\\s+").length)) * 100.0);
         return matchPercent;
     }
-    
-    void arrPrinter(){
-        for(int i = 0; i<getFileCount(); i++){
+
+    void wordCountingMapsPrinter() {
+        for (int i = 0; i < getFileCount(); i++) {
             System.out.println(files[i]);
-            HashMap<String, Integer> current = arr.get(i);
+            HashMap<String, Integer> current = wordCountMaps.get(i);
             for (String j : current.keySet()) {
                 System.out.println("key: " + j + "; value: " + current.get(j));
-              }
+            }
         }
     }
 
--- TestingDriver.java
+++ TestingDriver.java
@@ -7,11 +7,11 @@ public class TestingDriver
         fileReader.wordCountingMaps();
         fileReader.amassTexts();
         fileReader.wordCounterMapper();
-        fileReader.arrPrinter();
-        String textB = "It is an important and popular fact that things are not always what they seem. It is well recognised that things are not always what they seem. as said 'this is a good test' in the book and they say 'this is a good test'";
-        String textA = "It is well recognised that things are not always what they seem. Spoken 'this is a good test'";
-        double matchPercent = fileReader.phraseMatcher(textA, textB);
-        System.out.println("This is the matching percent for the file:");
-        System.out.println(matchPercent + "%");
+        fileReader.wordCountingMapsPrinter();
+        fileReader.filePair();
+        fileReader.printFilePair();
+        fileReader.setPhraseMinWordCount();
+        fileReader.filePairMatches();
+        fileReader.printMatchPercents();
     }
 }
